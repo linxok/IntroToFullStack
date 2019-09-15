8920fb75b825235ce529726d4b2a693d6f9ab483
LinxOK <linxok@gmail.com>
2019-09-15 23:03:35 +0300

    landing, add key up and menu anker

---
+++ WD_PS_3_jQuery/Landing/script/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() !== 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').on( 'click', function () {
+        $('body,html').animate({scrollTop: 0}, 1500);
+    });
+
+    $("#menu").on("click","a", function (event) {
+
+        event.preventDefault();
+
+        const id  = $(this).attr('href');
+
+        const top = $(id).offset().top;
+
+        $("body,html").animate({scrollTop: top}, 1500);
+    });
+});
+
