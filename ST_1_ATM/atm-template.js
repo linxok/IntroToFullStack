08ae8c0f54a809cc0bf946063e1875dbca543597
LinxOK <linxok@gmail.com>
2019-12-14 23:35:55 +0200

    ST1 дописаны функции. проведены тесты

--- ST_1_ATM/atm-template.js
+++ ST_1_ATM/atm-template.js
@@ -1,6 +1,8 @@
 const ATM = {
   isAuth: false,
   currentUser: {},
+  // logging
+  logATM: [],
   // all cash available in ATM
   cash: 2000,
   // all available users
@@ -11,12 +13,18 @@ const ATM = {
   // authorization
   auth(id, pin) {
 
-    const user = this.users.find(empti => (empti.id === id) && (empti.pin === pin));
+    if (this.isAuth) {
+      console.log(`${this.currentUser.id}, ${this.currentUser.type} is already logged.`);
+      return;
+    }
+
+    const user = this.users.find(entry => (entry.id === id) && (entry.pin === pin));
 
     if (user) {
       this.currentUser = user;
       this.isAuth = true;
       console.log(`Authorise ${user.id}, ${user.type}`);
+      this.logATM.push(`Authorise ${user.id}, ${user.type}`);
     } else {
       console.log("Authorize filed.");
     }
@@ -24,45 +32,69 @@ const ATM = {
   // check current debet
   check() {
 
-    if (this.isAuth) {
+
+    if (this.isAuth && !(this.currentUser.type ==='admin')) {
       console.log(`balance: ${this.currentUser.debet} `);
+      this.logATM.push(`id: ${this.currentUser.id}, type: ${this.currentUser.type}, current balance ${this.currentUser.debet}`);
     } else {
-      console.log("WARNING, ASSES DENIED");
+      console.log("Ned authorized user");
     }
   },
   // get cash - available for user only
   getCash(amount) {
-    if (this.currentUser.type === 'admin' && !this.isAuth) {
+    if ((this.currentUser.type === 'admin') || !this.isAuth) {
       console.log("This operation only authorized users");
       return;
     }
     if (this.currentUser.debet < amount) {
       console.log("You do not have enough funds");
       return;
-    } else if (this.cash < amount){
+    } else if (this.cash < amount) {
       console.log("No mo cash in ATM");
       return;
     }
-
     this.currentUser.debet -= amount;
     this.cash -= amount;
-
     console.log(this.currentUser.debet);
+    this.logATM.push(`id: ${this.currentUser.id}, type: ${this.currentUser.type}, get cash ${amount}, current balance ${this.currentUser.debet}`);
   },
   // load cash - available for user only
   loadCash(amount) {
-
+    if ((this.currentUser.type === 'admin') || !this.isAuth) {
+      console.log("This operation only authorized users");
+      return;
+    }
+    this.currentUser.debet += amount;
+    this.cash += amount;
+    console.log(`id: ${this.currentUser.id}, type: ${this.currentUser.type}, get cash ${amount}, current balance ${this.currentUser.debet}`);
+    this.logATM.push(`id: ${this.currentUser.id}, type: ${this.currentUser.type}, load cash ${amount}, current balance ${this.currentUser.debet}`);
   },
   // load cash to ATM - available for admin only - EXTENDED
   loadAtmCash(amount) {
-
+    if (!(this.currentUser.type === 'admin')) {
+      console.log("This operation only authorized Admin");
+      return;
+    }
+    this.cash += amount;
+    console.log(`ATM load cash: ${amount}, thea cash is ${this.cash}.`);
   },
   // get cash actions logs - available for admin only - EXTENDED
   getLogs() {
-
+    if (this.currentUser.type === 'admin') {
+      this.logATM.forEach(function (entry) {
+        console.log(entry.toString());
+      })
+    } else console.log("Asses denied");
   },
   // log out
   logout() {
-
+    if (!this.isAuth) {
+      console.log("no one to log out");
+      return;
+    }
+    console.log(`Type: ${this.currentUser.type} id: ${this.currentUser.id} is logout.`);
+    this.logATM.push(`Type: ${this.currentUser.type} id: ${this.currentUser.id} is logout.`);
+    this.isAuth = false;
+    this.currentUser = {};
   }
 };
