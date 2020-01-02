c284c6adc0383679723ddb872d5245f915967205
LinxOK <linxok@gmail.com>
2020-01-02 13:19:21 +0200

    исправления ко второму ревю

--- WD_PS_3_jQuery/Landing/script/main.js
+++ WD_PS_3_jQuery/Landing/script/main.js
@@ -1,4 +1,8 @@
+
+
 $(document).ready(function () {
+  const TIME_ANIMATE = 1500;
+  const WINDOW_POSITION =2; // 2- position center window
 
     $(window).scroll(function () {
         if ($(this).scrollTop() !== 0) {
@@ -8,15 +12,15 @@ $(document).ready(function () {
         }
     });
     $('#toTop').on( 'click', function () {
-        $('body,html').animate({scrollTop: 0}, 1500);
+        $('body,html').animate({scrollTop: 0}, TIME_ANIMATE);
     });
 
     $("#menu").on("click","a", function (event) {
         event.preventDefault();
         const id  = $(this).attr('href');
-        const top = $(id).offset().top - ($(window).height() /2);
+        const top = $(id).offset().top - ($(window).height() / WINDOW_POSITION);
 
-        $("body,html").animate({scrollTop: top}, 1500);
+        $("body,html").animate({scrollTop: top}, TIME_ANIMATE);
     });
 });
 
